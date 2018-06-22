dd4a8bb74fc2a4979cdaa549285966d42bfe15ea
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2018-06-22 09:41:43 +0200

    Add comments to flame.sh

--- flame.sh
+++ flame.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Get the actual Node.js process. The top one is `bash -c` process to execute Node.js.
 host_node_pid=$(pgrep node -f | tail -n 1)
+# Get a container ID made from `perf-test` image.
 container_id=$(sudo docker ps --format '{{.ID}}' --filter 'ancestor=perf-test')
+# Get the actual Node.js process from the Docker container.
 container_node_pid=$(sudo docker exec ${container_id} pgrep node -f | tail -n 1)
 
 echo container id ${container_id}
