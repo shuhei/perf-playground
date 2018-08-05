ff17bfebee7eda2f1822d59636158d9f86fae02f
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2018-08-05 22:34:41 +0200

    Make flamegraph with 0x

--- flame.sh
+++ flame.sh
@@ -6,6 +6,7 @@ host_node_pid=$(pgrep node -f | tail -n 1)
 container_id=$(sudo docker ps --format '{{.ID}}' --filter 'ancestor=perf-test')
 # Get the actual Node.js process from the Docker container.
 container_node_pid=$(sudo docker exec ${container_id} pgrep node -f | tail -n 1)
+0x_dir=0x-${host_node_pid}
 
 echo container id ${container_id}
 echo host node pid ${host_node_pid}
@@ -14,8 +15,14 @@ echo container node pid ${container_node_pid}
 echo "Recording CPU usage"
 sudo perf record -F 99 -p ${host_node_pid} -g -- sleep 30s
 
+# TODO: Who reads this file? And when? Probably `perf script`?
 echo "Copy symbol data"
 sudo docker cp ${container_id}:/tmp/perf-${container_node_pid}.map /tmp/perf-${host_node_pid}.map
 
 echo "Generate flame graph"
 sudo perf script | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl --colors js > out.svg
+
+# Make a dedidated directory because 0x reads all the files in a given directory.
+mkdir ${0x_dir}
+sudo perf script > ${0x_dir}/stacks.${host_node_pid}.out
+0x --visualize-only ${0x_dir}
